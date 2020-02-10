7d87b94b0ef4834049b099bd32dd69d5ca523c58
silviomdelgado <silviomdelgado@gmail.com>
2020-02-10 13:43:26 -0200

    Adding new methods and refactoring

--- atomical.js
+++ atomical.js
@@ -1,53 +1,74 @@
 /*!
-  * AtomicalJS v1.0 - Vanilla Javascript Ultra Light jQuery alternative.
+  * AtomicalJS v1.1 - Vanilla Javascript Ultra Light jQuery alternative.
   * Copyright 2019 Silvio Delgado (https://github.com/silviodelgado)
   * Licensed under MIT (https://opensource.org/licenses/MIT)
   * https://github.com/silviodelgado/AtomicalJS
   */
 var $ = function (selector, context) {
 
-    var workObj = {
-        element: null,
+    var internal = {
+        $element: null,
+        $request: null,
         on: function (eventName, callback) {
             if (typeof callback === 'function') {
-                workObj.element.addEventListener(eventName, callback);
+                internal.$element.addEventListener(eventName, callback);
             }
-            return workObj;
+            return internal;
+        },
+        ready: function (callback) {
+            if (typeof callback === 'function') {
+                if (document.readyState != 'loading') {
+                    callback();
+                } else {
+                    internal.$element.addEventListener('DOMContentLoaded', callback);
+                }
+            }
+            return internal;
         },
         off: function () {
-            workObj.element.parentNode.replaceChild(workObj.element.cloneNode(true), workObj.element);
-            return workObj;
+            internal.$element.parentNode.replaceChild(internal.$element.cloneNode(true), internal.$element);
+            return internal;
         },
-        attr: function (name, value) {
+        attr: function (attributeName, value) {
             if (value !== undefined) {
-                workObj.element.setAttribute(name, value);
-                return workObj;
+                internal.$element.setAttribute(attributeName, value);
+                return internal;
             }
-            return workObj.element.getAttribute(name);
+            return internal.$element.getAttribute(attributeName);
+        },
+        removeAttr: function (attributeName) {
+            internal.$element.removeAttribute(attributeName);
+            return internal;
         },
         hasClass: function (className) {
-            return workObj.element.className.indexOf(className) >= 0;
+            return internal.$element.className.indexOf(className) >= 0;
         },
         addClass: function (className) {
-            workObj.element.className += ' ' + className;
-            return workObj;
+            //internal.$element.className += ' ' + className;
+            internal.$element.classList.add(className);
+            return internal;
         },
         removeClass: function (className) {
-            workObj.element.className = workObj.element.className
-                .replace(className, '').replace('  ', ' ').trim();
-            return workObj;
+            internal.$element.classList.remove(className);
+            //internal.$element.className = internal.$element.className
+            //    .replace(className, '').replace('  ', ' ').trim();
+            return internal;
         },
         toggleClass: function (className) {
-            if (workObj.hasClass(className)) {
-                workObj.removeClass(className);
-            } else {
-                workObj.addClass(className);
-            }
-            return workObj;
+            internal.$element.classList.toggle(className);
+            //if (internal.hasClass(className)) {
+            //    internal.removeClass(className);
+            //} else {
+            //    internal.addClass(className);
+            //}
+            return internal;
         },
         css: function (property, style) {
-            workObj.element.style[property] = style;
-            return workObj;
+            if (typeof style !== undefined) {
+                internal.$element.style[property] = style;
+                return internal;
+            }
+            return internal.$element.style[property];
         },
         ajax: function (params) {
             var values = {
@@ -58,12 +79,11 @@ var $ = function (selector, context) {
                 error: params.error || null,
                 exception: params.exception || null
             };
-
-            var request = new XMLHttpRequest();
-            request.open(values.method, values.url, true);
-            request.onreadystatechange = function () {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
+            internal.$request = new XMLHttpRequest();
+            internal.$request.open(values.method, values.url, true);
+            internal.$request.onreadystatechange = function (evt) {
+                if (internal.$request.readyState === 4) {
+                    if (internal.$request.status === 200) {
                         if (typeof values.success === 'function') {
                             values.success(JSON.parse(this.response));
                         }
@@ -72,19 +92,32 @@ var $ = function (selector, context) {
                             values.error(this);
                         }
                     }
+
+                    for (var method in window.onAjaxComplete) {
+                        if (typeof window.onAjaxComplete[method] === 'function') {
+                            window.onAjaxComplete[method](this);
+                        }
+                    }
                 }
             }
-            request.onerror = function () {
+            internal.$request.onerror = function () {
                 if (typeof values.exception === 'function') {
                     values.exception();
                     return;
                 }
                 throw new Error(this.response);
             }
-            request.send(values.params);
+
+            for (var method in window.onAjaxStart) {
+                if (typeof window.onAjaxStart[method] === 'function') {
+                    window.onAjaxStart[method](this);
+                }
+            }
+            internal.$request.send(values.params);
+            return internal;
         },
         post: function (url, params, callback_success, callback_error, callback_exception) {
-            workObj.ajax({
+            internal.ajax({
                 method: 'POST',
                 url,
                 params: params || {},
@@ -92,9 +125,10 @@ var $ = function (selector, context) {
                 error: callback_error,
                 exception: callback_exception
             });
+            return internal;
         },
         get: function (url, callback_success, callback_error, callback_exception) {
-            workObj.ajax({
+            internal.ajax({
                 method: 'GET',
                 url,
                 params: {},
@@ -102,40 +136,99 @@ var $ = function (selector, context) {
                 error: callback_error,
                 exception: callback_exception
             });
+            return internal;
+        },
+        ajaxStart: function (callback) {
+            if (typeof callback === 'function') {
+                if (typeof window.onAjaxStart === 'undefined') {
+                    window.onAjaxStart = [];
+                }
+                window.onAjaxStart.push(callback);
+            }
+            return internal;
+        },
+        ajaxComplete: function (callback) {
+            if (typeof callback === 'function') {
+                if (typeof window.onAjaxComplete === 'undefined') {
+                    window.onAjaxComplete = [];
+                }
+                window.onAjaxComplete.push(callback);
+            }
+            return internal;
         },
         appendTo: function (appendSelector, appendContext) {
             var appendObj = getSelector(appendSelector, appendContext);
-            appendObj.element.appendChild(workObj.element);
+            appendObj.$element.appendChild(internal.$element);
+            return internal;
         },
         append: function (domStructure) {
             var outer = document.createElement('div');
             outer.innerHTML = domStructure;
-            workObj.element.appendChild(outer.children[0]);
-            return workObj;
+            internal.$element.appendChild(outer.children[0]);
+            return internal;
+        },
+        remove: function () {
+            internal.$element.parentNode.removeChild(internal.$element);
+        },
+        parent: function () {
+            return internal.$element.parentNode;
+        },
+        trigger: function (eventName, eventDetail) {
+            if (window.CustomEvent && typeof window.CustomEvent === 'function') {
+                var event = new CustomEvent(eventName, { detail: eventDetail });
+            } else {
+                var event = document.createEvent('CustomEvent');
+                event.initCustomEvent(eventName, true, true, eventDetail);
+            }
+            internal.$element.dispatchEvent(event);
+        },
+        focus: function () {
+            internal.$element.focus();
+        },
+        show: function () {
+            internal.$element.style.display = '';
+        },
+        hide: function () {
+            internal.$element.style.display = 'none';
+        },
+        html: function (content) {
+            if (typeof content !== undefined) {
+                internal.$element.innerHTML = content;
+                return internal;
+            }
+            return internal.$element.innerHTML;
+        },
+        text: function (content) {
+            if (typeof content !== undefined) {
+                internal.$element.textContent = content;
+                return internal;
+            }
+            return internal.$element.textContent;
         },
         extend: function (target, source) {
             if (typeof target !== 'object' || typeof source !== 'object') return false;
+            var workObj = Object.assign({}, target);
             for (var prop in source) {
                 if (!source.hasOwnProperty(prop)) continue;
-                if (prop in target) {
-                    if (typeof target[prop] !== 'object') {
-                        target[prop] = source[prop];
+                if (prop in workObj) {
+                    if (typeof workObj[prop] !== 'object') {
+                        workObj[prop] = source[prop];
                     } else {
                         if (typeof source[prop] !== 'object') {
-                            target[prop] = source[prop];
+                            workObj[prop] = source[prop];
                         } else {
-                            if (target[prop].concat && source[prop].concat) { 
-                                target[prop] = target[prop].concat(source[prop]);
+                            if (workObj[prop].concat && source[prop].concat) {
+                                workObj[prop] = targworket[prop].concat(source[prop]);
                             } else {
-                                target[prop] = workObj.extend(target[prop], source[prop]);
+                                workObj[prop] = internal.extend(workObj[prop], source[prop]);
                             }
                         }
                     }
                 } else {
-                    target[prop] = source[prop];
+                    workObj[prop] = source[prop];
                 }
             }
-            return target;
+            return workObj;
         }
     };
 
@@ -145,7 +238,7 @@ var $ = function (selector, context) {
                 window.setTimeout(selector);
             else
                 document.addEventListener('DOMContentLoaded', selector);
-            return workObj.element;
+            return internal.$element;
         }
 
         if (typeof selector === 'string' && selector.indexOf('<') === 0) {
@@ -164,7 +257,7 @@ var $ = function (selector, context) {
         }
     }
 
-    workObj.element = getSelector(selector, context);
+    internal.$element = getSelector(selector, context);
 
-    return workObj;
+    return internal;
 }
--- playground/playground.js
+++ playground/playground.js
@@ -6,6 +6,9 @@ $(function () {
         //console.log('$(this)', $(this));
 
         $('div').toggleClass('red');
+
+        $('button.extend').trigger('click');
+        $('button.extend').focus();
     });
     $('button.attribute').on('click', function (evt) {
         evt.preventDefault();
@@ -13,7 +16,7 @@ $(function () {
         $(this).attr('tabindex', '-1');
         $(this).attr('disabled', 'disabled');
 
-        $().get('http://atomicaljs.test/items.json', function (r) {
+        $().get('http://atomicaljs.test/playground/items.json', function (r) {
             console.log('response from ajax: ', r.data);
         });
 
@@ -48,7 +51,30 @@ $(function () {
             }
         }
         
-        $().extend(a, b);
+        console.log('$', $);
+        var c = $().extend(a, b);
         console.log('a', a);
+        console.log('b', b);
+        console.log('c', c);
+    });
+
+    var a = $(document).ready(function () {
+        console.log('ready 2 a');
+        $('button.extend').focus();
+    });
+    //console.log('a', a);
+
+    var b = $(document).ajaxStart(function () {
+        console.log('ajax start 1');
+    });
+    //console.log('b', b);
+
+    $(document).ready(function () {
+        console.log('ready');
+        //alert('Ready!');
+    }).ajaxStart(function () {
+        console.log('ajax start');
+    }).ajaxComplete(function () {
+        console.log('ajax complete');
     });
 });
